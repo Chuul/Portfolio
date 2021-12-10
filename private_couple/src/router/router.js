ac13cf666e175fc13d28f4fb4fd865885b4cf351
Chuul <bethejs30@gmail.com>
2021-12-10 15:22:36 +0900

    Refactoring: Mixin 적용

--- private_couple/src/router/router.js
+++ private_couple/src/router/router.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router';
 import DateView from '../Views/DateCourse/DateView.vue'
 import CourseListView from '../Views/CourseListView.vue'
 import LastCourseView from '../Views/LastCourseView.vue'
-import StartCourseView from '../Views/StartCourseView.vue'
+import startCourseView from '../Views/StartCourseView.vue'
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
@@ -14,7 +14,7 @@ export const router = new VueRouter({
     },
     {
       path : '/createCourse',
-      component : DateView,
+      component : DateView
     },
     {
       path : '/courseList',
@@ -26,7 +26,7 @@ export const router = new VueRouter({
     },
     {
       path : '/startCourse',
-      component : StartCourseView,
+      component : startCourseView
     }
   ]
 })
