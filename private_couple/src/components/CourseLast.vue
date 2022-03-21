063863039b3538ac8db1a6d3432a44946e7f581c
Chuul <bethejs30@gmail.com>
2022-03-21 12:49:05 +0900

    Style: 이전 코스 반응형 정리 변경원인: 이전 코스 페이지의 반응형 처리가 미흡 해결방법: 아이템과 코스의 컨테이너의 높이, 이름, comment의 높이, 넓이를 지정해줌

--- private_couple/src/components/CourseLast.vue
+++ private_couple/src/components/CourseLast.vue
@@ -1,30 +1,39 @@
 <template>
 	<section class="last_cont">
 		<article class="item_cont">
-			<h3>이벤트</h3>
-			<li v-for="item in ItemList" :key="item">
-				<span class="item_name">
-					{{ item.name }}
-				</span>
-				<span v-if="item.comment !== ''">
-					{{ item.comment }}
-				</span>
-				<span v-else> 평가가 없습니다... </span>
-			</li>
+			<h3>아이템</h3>
+			<article>
+				<li v-for="item in ItemList" :key="item">
+					<span class="item_name">
+						{{ item.name }}
+					</span>
+					<span class="item_comment" v-if="item.comment !== ''">
+						{{ item.comment }}
+					</span>
+					<span class="item_comment" v-else> 평가가 없습니다... </span>
+				</li>
+			</article>
 		</article>
 		<article class="course_cont">
 			<h3>코스</h3>
-			<li v-for="course in CourseList" :key="course.list">
-				<div class="course_item_cont">
-					<li v-for="item in course.list" class="course_name" :key="item.name">
-						{{ item.name }}
-						<i class="fas fa-arrow-right" />
-					</li>
-				</div>
-				<span>
-					{{ course.comment }}
-				</span>
-			</li>
+			<article>
+				<li v-for="course in CourseList" :key="course.list">
+					<div class="course_item_cont">
+						<li
+							v-for="item in course.list"
+							class="course_name"
+							:key="item.name"
+						>
+							{{ item.name }}
+							<i class="fas fa-arrow-right" />
+						</li>
+					</div>
+					<span class="course_comment" v-if="course.comment !== ''">
+						{{ course.comment }}
+					</span>
+					<span class="course_comment" v-else> 평가가 없습니다... </span>
+				</li>
+			</article>
 		</article>
 	</section>
 </template>
@@ -49,9 +58,11 @@ export default {
 li {
 	list-style: none;
 	margin-bottom: 0.5em;
+	display: flex;
+	justify-content: space-between;
 }
 h3 {
-	margin-bottom: 1.5em;
+	margin: 0 0 1.5em;
 	font-family: 'Noto Sans KR', sans-serif;
 	font-weight: 500;
 }
@@ -60,32 +71,41 @@ h3 {
 	margin: 0 1rem;
 }
 .item_cont {
+	max-height: 40%;
+	overflow: auto;
 	text-align: center;
-	margin: 15px;
 	padding: 1rem;
-	background: rgba(124, 198, 255, 0.11);
+	background: rgba(124, 198, 255, 0.247);
 	border-radius: 0.5em;
 	box-shadow: 0.5em -0.3em 10px 1px rgba(143, 143, 143, 0.2);
 }
 .item_name {
+	width: 25%;
 	float: left;
 }
+.item_comment {
+	width: 60%;
+}
 .course_cont {
+	max-height: 40%;
+	overflow: auto;
 	text-align: center;
-	margin: 15px;
+	margin-top: 1rem;
 	padding: 1rem;
-	background: rgba(124, 198, 255, 0.11);
+	background: rgba(124, 198, 255, 0.247);
 	border-radius: 0.5em;
 	box-shadow: 0.5em -0.3em 10px 1px rgba(143, 143, 143, 0.2);
 }
 .course_item_cont {
-	display: inline;
-	float: left;
+	width: 40%;
 }
 .course_item_cont li:last-child i {
 	display: none;
 }
 .course_name {
 	display: inline;
+}
+.course_comment {
+	width: 55%;
 }
 </style>
